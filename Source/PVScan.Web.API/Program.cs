dff4eba4cd29bf95932b30d542d6b04294cafc12
Michael <mejevin@gmail.com>
2020-07-26 17:18:25 +0300

    refactor: create empty ASP.NET core 3.1 project

--- Source/PVScan.Web.API/Program.cs
+++ Source/PVScan.Web.API/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace PVScan.Web
+namespace PVScan.Web.API
 {
     public class Program
     {
