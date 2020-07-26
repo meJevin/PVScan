249814a9d360649b8cc2c2ccd2317009808c97b4
Michael <mejevin@gmail.com>
2020-07-26 16:00:19 +0300

    add: blank asp.net core webAPI project

---
+++ Source/PVScan.Web/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PVScan.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> Index()
+        {
+            return "Hello world! From API controller!";
+        }
+    }
+}
