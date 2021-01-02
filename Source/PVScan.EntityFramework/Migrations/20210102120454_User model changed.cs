0c6ae619c31b6f869c03d676fe5257f059f58845
Michael <mejevin@gmail.com>
2021-01-02 15:05:11 +0300

    add: migration for user model change

---
+++ Source/PVScan.EntityFramework/Migrations/20210102120454_User model changed.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PVScan.EntityFramework.Migrations
+{
+    public partial class Usermodelchanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Barcodes_Users_ScannedByEmail",
+                table: "Barcodes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Barcodes_ScannedByEmail",
+                table: "Barcodes");
+
+            migrationBuilder.DropColumn(
+                name: "ScannedByEmail",
+                table: "Barcodes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsExternal",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ScannedById",
+                table: "Barcodes",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Barcodes_ScannedById",
+                table: "Barcodes",
+                column: "ScannedById");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Barcodes_Users_ScannedById",
+                table: "Barcodes",
+                column: "ScannedById",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Barcodes_Users_ScannedById",
+                table: "Barcodes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Barcodes_ScannedById",
+                table: "Barcodes");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsExternal",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ScannedById",
+                table: "Barcodes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ScannedByEmail",
+                table: "Barcodes",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Email");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Barcodes_ScannedByEmail",
+                table: "Barcodes",
+                column: "ScannedByEmail");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Barcodes_Users_ScannedByEmail",
+                table: "Barcodes",
+                column: "ScannedByEmail",
+                principalTable: "Users",
+                principalColumn: "Email",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
