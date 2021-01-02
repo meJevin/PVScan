c9b0a886b61826c44fe8a11e41db80cd079d8bcc
Michael <mejevin@gmail.com>
2021-01-02 05:46:20 +0300

    add: basic models in domain project

---
+++ Source/PVScan.Domain/Models/Coordinate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PVScan.Domain.Models
+{
+    public class Coordinate
+    {
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+    }
+}
---
+++ Source/PVScan.Domain/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PVScan.Domain.Models
+{
+    public class User
+    {
+        public string Email;
+
+        public string Username;
+
+        public ICollection<Barcode> ScannedBarcodes;
+    }
+}
