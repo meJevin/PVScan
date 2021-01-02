5d6b4c853585802877c1fcf1abb4b7a17c177f64
Michael <mejevin@gmail.com>
2021-01-02 05:54:30 +0300

    add: barcode service that creates a barcode

---
+++ Source/PVScan.Domain/Services/IBarcodeService.cs
@@ -0,0 +1,13 @@
+﻿using PVScan.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PVScan.Domain.Services
+{
+    public interface IBarcodeService
+    {
+        Task<Barcode> Create(Barcode barcode);
+    }
+}
