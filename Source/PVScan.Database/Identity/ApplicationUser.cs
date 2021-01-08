c2cf1dd71eeff1afaf9c2a2203d5a82492a759aa
Michael <mejevin@gmail.com>
2021-01-08 12:12:01 +0300

    dbcontext

---
+++ Source/PVScan.Database/Identity/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PVScan.Database.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
