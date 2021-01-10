9afee64d5bc9b66a67f4f90264d80badb86fd6fc
Michael <mejevin@gmail.com>
2021-01-10 21:55:06 +0300

    add: userinfo level info

--- Source/PVScan.Domain/Entities/UserInfo.cs
+++ Source/PVScan.Domain/Entities/UserInfo.cs
@@ -7,10 +7,12 @@ namespace PVScan.Domain.Entities
     public class UserInfo
     {
         public int Id { get; set; }
+        public string UserId { get; set; }
         public int BarcodesScanned { get; set; }
         public int BarcodeFormatsScanned { get; set; }
+        public double Experience { get; set; }
+        public int Level { get; set; }
         public string VKLink { get; set; }
         public string IGLink { get; set; }
-        public string UserId { get; set; }
     }
 }
