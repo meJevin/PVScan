d0c5f75428b0bd9586a9690964c08591b76277a8
Michael <mejevin@gmail.com>
2021-01-10 21:55:20 +0300

    add: expreience calculator service

---
+++ Source/PVScan.API/Services/ExperienceCalculator.cs
@@ -0,0 +1,39 @@
+﻿using PVScan.API.Services.Interfaces;
+using PVScan.Database;
+using PVScan.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PVScan.API.Services
+{
+    public class ExperienceCalculator : IExperienceCalculator
+    {
+        readonly PVScanDbContext _context;
+
+        public ExperienceCalculator(PVScanDbContext context)
+        {
+            _context = context;
+        }
+
+        public double GetExperienceForBarcode(UserInfo info)
+        {
+            // TODO:  Make this a little more interesting using DbContext to fetch some info
+            double result = GetRequiredLevelExperience(info.Level) * 0.1;
+
+            return result;
+        }
+
+        public double GetRequiredLevelExperience(int level)
+        {
+            double result = 200;
+
+            for (int i = 1; i < level; ++i)
+            {
+                result += 100 * i;
+            }
+            
+            return result;
+        }
+    }
+}
