de2853123530dc89fa6d248d95c24021fbeb4492
Michael <mejevin@gmail.com>
2021-01-10 15:18:38 +0300

    change: add base controller with access to current userId

---
+++ Source/PVScan.API/Controllers/Base/PVScanBaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace PVScan.API.Controllers.Base
+{
+    [ApiController]
+    public class PVScanBaseController : ControllerBase
+    {
+        protected string UserId =>
+            HttpContext.User.Claims.Where(c => c.Type == ClaimTypes.NameIdentifier)
+                .Select(c => c.Value)
+                .FirstOrDefault();
+    }
+}
