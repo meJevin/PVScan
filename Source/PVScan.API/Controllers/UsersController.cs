7911396bbf891072d142ac2a751451826d3c9689
Michael <mejevin@gmail.com>
2021-01-10 15:06:11 +0300

    add: user info and user controller

---
+++ Source/PVScan.API/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PVScan.API.ViewModels.Users;
+using PVScan.Database;
+using PVScan.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace PVScan.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        PVScanDbContext _context;
+
+        public UsersController(PVScanDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("info")]
+        public async Task<IActionResult> Info(InfoViewModel data)
+        {
+            var userInfo = await _context.UserInfos
+                .Where(u => u.UserId == data.UserId)
+                .FirstOrDefaultAsync();
+
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(userInfo);
+        }
+    }
+}
---
+++ Source/PVScan.API/ViewModels/Users/InfoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PVScan.API.ViewModels.Users
+{
+    public class InfoViewModel
+    {
+        public string UserId;
+    }
+}
