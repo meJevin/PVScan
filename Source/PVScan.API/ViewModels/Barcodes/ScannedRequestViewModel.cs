0e6e945215ce9f9565a284c6fe48ceae09821930
Michael <mejevin@gmail.com>
2021-01-10 22:51:43 +0300

    change: Barcode scanned req/res view models

---
+++ Source/PVScan.API/ViewModels/Barcodes/ScannedRequestViewModel.cs
@@ -0,0 +1,16 @@
+﻿using PVScan.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PVScan.API.ViewModels.Barcodes
+{
+    public class ScannedRequestViewModel
+    {
+        public string Text { get; set; }
+        public BarcodeFormat Format { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
---
+++ Source/PVScan.API/ViewModels/Barcodes/ScannedResponseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PVScan.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PVScan.API.ViewModels.Barcodes
+{
+    public class ScannedResponseViewModel
+    {
+        public double ExperienceGained { get; set; }
+        public Barcode Barcode { get; set; }
+    }
+}
