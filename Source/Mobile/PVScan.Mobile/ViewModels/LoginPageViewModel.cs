b1981704b3c036f8d7310ebc4e8769f8864ea8cf
Michael <mejevin@gmail.com>
2021-01-17 01:22:52 +0300

    add: mvvm to login/sign up pages temp

---
+++ Source/Mobile/PVScan.Mobile/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,56 @@
+﻿using IdentityModel.Client;
+using MvvmHelpers;
+using PVScan.Mobile.Services.Identity;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using static IdentityModel.OidcConstants;
+
+namespace PVScan.Mobile.ViewModels
+{
+    public class LoginPageViewModel : BaseViewModel
+    {
+        public LoginPageViewModel()
+        {
+            LoginCommand = new Command(async () =>
+            {
+                HttpClient httpClient = new HttpClient();
+
+                // Todo: enable https in production
+                DiscoveryDocumentResponse discoveryDocument = 
+                    await httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest() 
+                    {
+                        Address = IdentityServerConfiguration.Authority,
+                        Policy = { RequireHttps = false },
+                    });
+
+                var token = await httpClient.RequestPasswordTokenAsync(new PasswordTokenRequest()
+                {
+                    Address = discoveryDocument.TokenEndpoint,
+                    ClientId = "PVScan.Auth.Mobile",
+                    GrantType = GrantTypes.Password,
+                    Scope = "openid profile PVScan.API",
+                    Password = Password,
+                    UserName = Login,
+                });
+
+                if (token.AccessToken != null)
+                {
+                    // Success
+                }
+                else
+                {
+
+                }
+            });
+        }
+
+        public ICommand LoginCommand { get; }
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Source/Mobile/PVScan.Mobile/ViewModels/SignUpPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using MvvmHelpers;
+using PVScan.Mobile.Services.Identity;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PVScan.Mobile.ViewModels
+{
+    public class SignUpPageViewModel : BaseViewModel
+    {
+        public SignUpPageViewModel()
+        {
+            SignUpCommand = new Command(async () =>
+            {
+                HttpClient httpClient = new HttpClient();
+                httpClient.BaseAddress = new Uri(IdentityServerConfiguration.Authority);
+
+                var content = new FormUrlEncodedContent(new Dictionary<string, string> 
+                {
+                    { "Username", Login },
+                    { "Password", Password },
+                    { "Email", Email },
+                });
+
+                var result = await httpClient.PostAsync("/Auth/Register", content);
+
+                if (result.IsSuccessStatusCode 
+                    || result.StatusCode == System.Net.HttpStatusCode.Redirect)
+                {
+                    // Success
+                }
+                else
+                {
+                    // Error
+                }
+            });
+        }
+
+        public ICommand SignUpCommand { get;  }
+
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
