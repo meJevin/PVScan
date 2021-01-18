c9c28611b62188ceb1730fb69c385646e6bc0068
Michael <mejevin@gmail.com>
2021-01-18 03:29:56 +0300

    add: API address

--- Source/Mobile/PVScan.Mobile/Services/Identity/IdentityServerConfiguration.cs
+++ Source/Mobile/PVScan.Mobile/Services/Identity/IdentityServerConfiguration.cs
@@ -10,5 +10,8 @@ namespace PVScan.Mobile.Services.Identity
         public static string Authority = Device.RuntimePlatform == Device.Android ? "http://10.0.2.2:1488" : "http://localhost:1488";
         public const string RedirectUri = "pvscan://callback";
         public const string ClientId = "PVScan.Auth.Mobile";
+
+        // Todo: this doesn't belong here
+        public static string API = Device.RuntimePlatform == Device.Android? "http://10.0.2.2:1337" : "http://localhost:1337";
     }
 }
