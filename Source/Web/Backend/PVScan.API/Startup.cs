3cb6af4c077685ae8925a4ff1e782ad9513598d7
Michael <mejevin@gmail.com>
2021-01-19 16:11:57 +0300

    change: launch settings option

--- Source/Web/Backend/PVScan.API/Startup.cs
+++ Source/Web/Backend/PVScan.API/Startup.cs
@@ -35,7 +35,7 @@ namespace PVScan.API
             services.AddAuthentication("Bearer")
                 .AddJwtBearer("Bearer", options =>
                 {
-                    options.Authority = "http://localhost:1488/";
+                    options.Authority = "http://192.168.1.74:1488/";
 
                     options.TokenValidationParameters = new TokenValidationParameters()
                     {
