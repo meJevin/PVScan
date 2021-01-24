178fc707b26f133e137dd6746c2b65f0232c60aa
Michael <mejevin@gmail.com>
2021-01-24 23:15:33 +0300

    Create Barcode.cs

---
+++ Source/Mobile/PVScan.Mobile/Models/Barcode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ZXing;
+
+namespace PVScan.Mobile.Models
+{
+    public class Barcode
+    {
+        public int Id { get; set; }
+        public BarcodeFormat Format { get; set; }
+        public string Text { get; set; }
+        public Coordinate ScanLocation { get; set; }
+        public bool ServerSynced { get; set; }
+    }
+}
