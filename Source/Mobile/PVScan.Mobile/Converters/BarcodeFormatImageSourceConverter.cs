f0070b5c466ef141fdc4c49e892b12471b60bbf8
Michael <mejevin@gmail.com>
2021-02-04 15:24:34 +0300

    add: converters for barcode visualisation

---
+++ Source/Mobile/PVScan.Mobile/Converters/BarcodeFormatImageSourceConverter.cs
@@ -0,0 +1,39 @@
+﻿using PVScan.Mobile.Styles;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+using ZXing;
+
+namespace PVScan.Mobile.Converters
+{
+    public class BarcodeFormatImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            BarcodeFormat format = (BarcodeFormat)value;
+
+            var result = new FontImageSource()
+            {
+                FontFamily = "FontAwesome",
+                Glyph = IconFont.Barcode,
+            };
+            result.SetOnAppTheme(FontImageSource.ColorProperty,
+                Application.Current.Resources["TextPrimaryColor_Light"],
+                Application.Current.Resources["TextPrimaryColor_Dark"]);
+
+            if (format.HasFlag(BarcodeFormat.QR_CODE))
+            {
+                result.Glyph = IconFont.Qrcode;
+            }
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ Source/Mobile/PVScan.Mobile/Converters/BarcodeFormatStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+using ZXing;
+
+namespace PVScan.Mobile.Converters
+{
+    public class BarcodeFormatStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            BarcodeFormat format = (BarcodeFormat)value;
+
+            return Enum.GetName(typeof(BarcodeFormat), format).Replace('_', ' ');
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
