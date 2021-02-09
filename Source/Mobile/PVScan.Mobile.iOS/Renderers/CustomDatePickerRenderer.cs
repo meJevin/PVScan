ecb3f00787c7f52631b751daa9bc86c7212a20e4
Michael <mejevin@gmail.com>
2021-02-09 21:52:14 +0300

    add: custom ios renderers with no border

---
+++ Source/Mobile/PVScan.Mobile.iOS/Renderers/CustomDatePickerRenderer.cs
@@ -0,0 +1,23 @@
+﻿using PVScan.Mobile.iOS.Renderers;
+using System.ComponentModel;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(DatePicker), typeof(CustomDatePickerRenderer))]
+namespace PVScan.Mobile.iOS.Renderers
+{
+    public class CustomDatePickerRenderer : DatePickerRenderer
+    {
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (Control != null)
+            {
+                Control.Layer.BorderWidth = 0;
+                Control.BorderStyle = UITextBorderStyle.None;
+            }
+        }
+    }
+}
