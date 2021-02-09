7424729c13a307433cb3ba3635a51b97dfcb847e
Michael <mejevin@gmail.com>
2021-02-09 21:27:43 +0300

    add: custom date picker renderer

---
+++ Source/Mobile/PVScan.Mobile.Android/Renderers/CustomDatePickerRenderer.cs
@@ -0,0 +1,60 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using PVScan.Mobile.Droid.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(DatePicker), typeof(CustomDatePickerRenderer))]
+namespace PVScan.Mobile.Droid.Renderers
+{
+    public class CustomDatePickerRenderer : DatePickerRenderer
+    {
+        public CustomDatePickerRenderer(Context context) : base(context)
+        {
+
+        }
+
+        void SetColor(Android.Graphics.Color color)
+        {
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                Control.BackgroundTintList = ColorStateList.ValueOf(color);
+            }
+            else
+            {
+                Control.Background.SetColorFilter(color, PorterDuff.Mode.SrcAtop);
+            }
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                SetColor(Android.Graphics.Color.Transparent);
+
+                this.EditText.FocusChange += (sender, ee) => {
+                    bool hasFocus = ee.HasFocus;
+                    if (hasFocus)
+                    {
+                        SetColor(Android.Graphics.Color.Transparent);
+                    }
+                    else
+                    {
+                        SetColor(Android.Graphics.Color.Transparent);
+                    }
+                };
+            }
+        }
+    }
+}
