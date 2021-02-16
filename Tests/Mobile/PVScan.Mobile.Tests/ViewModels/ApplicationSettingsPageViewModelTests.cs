fa65ee058295f049aee4bf0c2c0d4a110dba9694
Michael <mejevin@gmail.com>
2021-02-17 00:57:18 +0300

    add: unit tests for application settings page

---
+++ Tests/Mobile/PVScan.Mobile.Tests/ViewModels/ApplicationSettingsPageViewModelTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using PVScan.Mobile.DAL;
+using PVScan.Mobile.Models;
+using PVScan.Mobile.Services;
+using PVScan.Mobile.Services.Interfaces;
+using PVScan.Mobile.Tests.Services.Mocks;
+using PVScan.Mobile.ViewModels;
+using Xunit;
+
+namespace PVScan.Mobile.Tests.ViewModels
+{
+    public class ApplicationSettingsPageViewModelTests : TestBase
+    {
+        IPersistentKVP KVP;
+
+        public ApplicationSettingsPageViewModelTests()
+        {
+            KVP = new InMemoryKVP();
+        }
+
+        [Fact]
+        public void Theme_Set_To_Light_Displayed_In_Settings()
+        {
+            // Arrange
+            Xamarin.Forms.Application.Current.UserAppTheme = Xamarin.Forms.OSAppTheme.Light;
+
+            // Act
+            ApplicationSettingsPageViewModel vm = new ApplicationSettingsPageViewModel(KVP);
+
+            // Assert
+            Assert.False(vm.IsDarkTheme);
+        }
+
+        [Fact]
+        public void Theme_Set_To_Dark_Displayed_In_Settings()
+        {
+            // Arrange
+            Xamarin.Forms.Application.Current.UserAppTheme = Xamarin.Forms.OSAppTheme.Dark;
+
+            // Act
+            ApplicationSettingsPageViewModel vm = new ApplicationSettingsPageViewModel(KVP);
+
+            // Assert
+            Assert.True(vm.IsDarkTheme);
+        }
+
+        [Theory]
+        [InlineData("Dark", "Light")]
+        [InlineData("Light", "Dark")]
+        public void Theme_Toggled_Changes_Current_Theme(string oldTheme, string newTheme)
+        {
+            // Arrange
+            KVP.Clear();
+            KVP.Set(StorageKeys.Theme, oldTheme);
+            ApplicationSettingsPageViewModel vm = new ApplicationSettingsPageViewModel(KVP);
+
+            // Act
+            vm.IsDarkTheme = newTheme == "Dark";
+            vm.SwitchThemeCommand.Execute(null);
+
+            // Assert
+            Assert.Equal(newTheme, KVP.Get(StorageKeys.Theme, null));
+        }
+    }
+}
