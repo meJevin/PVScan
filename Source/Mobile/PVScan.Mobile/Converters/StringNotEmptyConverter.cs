f6fd203cd69d627ceb7cf7fa973db7c8d1b0d206
Michael <mejevin@gmail.com>
2021-02-25 21:41:31 +0300

    Create StringNotEmptyConverter.cs

---
+++ Source/Mobile/PVScan.Mobile/Converters/StringNotEmptyConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace PVScan.Mobile.Converters
+{
+    public class StringNotEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string text = value as string;
+
+            if (!String.IsNullOrEmpty(text))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
