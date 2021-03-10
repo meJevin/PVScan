6bcc27b5bbef37da1fc804ecec3b433da8649ab8
Michael <mejevin@gmail.com>
2021-03-11 00:57:28 +0300

    add: normal barcode item template

---
+++ Source/Mobile/PVScan.Mobile/Views/DataTemplates/NormalBarcodeItem.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PVScan.Mobile.Views.DataTemplates
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NormalBarcodeItem : Grid
+    {
+        public event EventHandler Tapped;
+
+        public NormalBarcodeItem()
+        {
+            InitializeComponent();
+        }
+
+        private void Barcode_Tapped(object sender, EventArgs e)
+        {
+            Tapped?.Invoke(sender, e);
+        }
+    }
+}
