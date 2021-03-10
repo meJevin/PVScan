00186fd125893d3ad5aeb678974b9938351f9888
Michael <mejevin@gmail.com>
2021-03-11 00:57:47 +0300

    add: selectable barcode item template

---
+++ Source/Mobile/PVScan.Mobile/Views/DataTemplates/SelectableBarcodeItem.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PVScan.Mobile.Views.DataTemplates
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SelectableBarcodeItem : Grid
+    {
+        public SelectableBarcodeItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
