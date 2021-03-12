2cdd4c8fba756f36a987b30e415640b3594eb3e1
Michael <mejevin@gmail.com>
2021-03-12 12:16:55 +0300

    add: Filter model

---
+++ Source/Mobile/PVScan.Mobile/Models/Filter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using ZXing;
+
+namespace PVScan.Mobile.Models
+{
+    // Enum for filter page to filter by last day/week/month/year
+    public enum LastTimeType
+    {
+        Day,
+        Week,
+        Month,
+        Year,
+    }
+
+    public class Filter
+    {
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+
+        public LastTimeType? LastType { get; set; }
+
+        public IEnumerable<BarcodeFormat> BarcodeFormats { get; set; }
+    }
+}
