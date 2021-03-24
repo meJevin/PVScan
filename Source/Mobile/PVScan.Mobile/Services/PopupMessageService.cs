c76855271a81ccd6e448fd3fa2c8c8563a6a4240
Michael <mejevin@gmail.com>
2021-03-24 15:13:50 +0300

    add: IPopupMessage service interface and its Xamarin.Forms implementation

---
+++ Source/Mobile/PVScan.Mobile/Services/PopupMessageService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using PVScan.Mobile.Services.Interfaces;
+using Xamarin.Forms;
+
+namespace PVScan.Mobile.Services
+{
+    public class PopupMessageService : IPopupMessageService
+    {
+        Grid PopupContainer;
+        Label PopupMessageLabel;
+
+        public void Initialize(Grid container, Label msgLabel)
+        {
+            PopupContainer = container;
+            PopupMessageLabel = msgLabel;
+        }
+
+        public async Task ShowMessage(string messageText)
+        {
+            PopupMessageLabel.Text = messageText;
+
+            await PopupContainer.FadeTo(1, 250, Easing.CubicOut);
+            await Task.Delay(1000);
+            await PopupContainer.FadeTo(0, 250, Easing.CubicOut);
+        }
+    }
+}
