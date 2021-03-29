7168b71da2ab69a1d25e058af57e8e5bd8713295
Michael <mejevin@gmail.com>
2021-03-29 11:01:54 +0300

    add: IFileBarcode reader to read barcodes from local files on system

---
+++ Source/Mobile/PVScan.Mobile/Services/Interfaces/IFileBarcodeReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using ZXing;
+
+namespace PVScan.Mobile.Services.Interfaces
+{
+    // Takes raw bytes of a picture (hopefully) and spits out a ZXING results
+    public interface IFileBarcodeReader
+    {
+        Task<Result> DecodeAsync(string filePath, Stream stream);
+    }
+}
