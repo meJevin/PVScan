d55ab4614b6fe39c00e937e9a8b472c5dae1f61b
Michael <mejevin@gmail.com>
2021-04-02 15:28:18 +0300

    change: PopupMessageService now animates container scale a little bit

--- Source/Mobile/PVScan.Mobile/Services/PopupMessageService.cs
+++ Source/Mobile/PVScan.Mobile/Services/PopupMessageService.cs
@@ -14,14 +14,21 @@ namespace PVScan.Mobile.Services
         {
             PopupContainer = container;
             PopupMessageLabel = msgLabel;
+
+            PopupContainer.Opacity = 0;
+            PopupContainer.Scale = 0.975;
         }
 
         public async Task ShowMessage(string messageText)
         {
             PopupMessageLabel.Text = messageText;
 
+            _ = PopupContainer.ScaleTo(1, 250, Easing.CubicOut);
             await PopupContainer.FadeTo(1, 250, Easing.CubicOut);
+
             await Task.Delay(1000);
+
+            _ = PopupContainer.ScaleTo(0.975, 250, Easing.CubicOut);
             await PopupContainer.FadeTo(0, 250, Easing.CubicOut);
         }
     }
