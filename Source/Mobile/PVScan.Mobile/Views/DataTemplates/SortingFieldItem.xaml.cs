90d7525cdccf94419605adc6413bf41337c63743
Michael <mejevin@gmail.com>
2021-04-11 01:39:18 +0300

    add: sorting field item control

---
+++ Source/Mobile/PVScan.Mobile/Views/DataTemplates/SortingFieldItem.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PVScan.Mobile.ViewModels;
+using Xamarin.Forms;
+
+namespace PVScan.Mobile.Views.DataTemplates
+{
+    public partial class SortingFieldItem : Grid
+    {
+        public static readonly BindableProperty SelectedSortingFieldProperty =
+            BindableProperty.Create(nameof(SelectedSortingField), typeof(SortingField), typeof(SortingFieldItem), null,
+                propertyChanged: OnCurrentSortingChanged);
+
+        public SortingField SelectedSortingField
+        {
+            get { return (SortingField)GetValue(SelectedSortingFieldProperty); }
+            set { SetValue(SelectedSortingFieldProperty, value); }
+        }
+
+        static async void OnCurrentSortingChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            SortingFieldItem i = (SortingFieldItem)bindable;
+
+            if ((SortingField)newValue == (SortingField)i.BindingContext)
+            {
+                await i.MakeSelected();
+            }
+            else
+            {
+                await i.MakeNotSelected();
+            }
+        }
+
+        public SortingFieldItem()
+        {
+            InitializeComponent();
+        }
+
+        public async Task MakeSelected()
+        {
+            await CheckImage.FadeTo(1, 250, Easing.CubicOut);
+        }
+
+        public async Task MakeNotSelected()
+        {
+            await CheckImage.FadeTo(0, 250, Easing.CubicOut);
+        }
+    }
+}
