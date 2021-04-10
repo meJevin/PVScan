716692a06988dde89686624f67015385a8481e91
Michael <mejevin@gmail.com>
2021-04-11 01:57:06 +0300

    add: sorting model with its enum for Sorting field

---
+++ Source/Mobile/PVScan.Mobile/Models/Sorting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace PVScan.Mobile.Models
+{
+    public enum SortingField
+    {
+        None,
+        Date,
+        Text,
+        Format
+    };
+
+    public class Sorting
+    {
+        public SortingField Field { get; set; }
+        public bool Descending { get; set; }
+
+        public static Sorting Default() => new Sorting()
+        {
+            Field = SortingField.Date,
+            Descending = true,
+        };
+    }
+}
