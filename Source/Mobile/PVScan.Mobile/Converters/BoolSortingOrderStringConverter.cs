b4fd47a236684eb5f9511bc500dcac2b1fa93015
Michael <mejevin@gmail.com>
2021-04-11 01:40:36 +0300

    add: BoolToSortingString converter

---
+++ Source/Mobile/PVScan.Mobile/Converters/BoolSortingOrderStringConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using PVScan.Mobile.ViewModels;
+using Xamarin.Forms;
+
+namespace PVScan.Mobile.Converters
+{
+    public class BoolSortingOrderStringConverter : IValueConverter
+    {
+        // True for descending false for ascending
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return "";
+            }
+
+            var desc = (bool)value;
+            return desc ? "descendingly" : "ascendingly"; 
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
