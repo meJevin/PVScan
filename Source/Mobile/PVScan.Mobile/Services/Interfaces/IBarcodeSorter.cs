84e5c1d1c845467473819c15c5fbb6dcc251b2ce
Michael <mejevin@gmail.com>
2021-04-11 01:57:22 +0300

    add: IBarcodeSorter interface

---
+++ Source/Mobile/PVScan.Mobile/Services/Interfaces/IBarcodeSorter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PVScan.Mobile.Models;
+
+namespace PVScan.Mobile.Services.Interfaces
+{
+    public interface IBarcodeSorter
+    {
+        Task<IEnumerable<Barcode>> Sort(IEnumerable<Barcode> barcodes, Sorting sorting);
+    }
+}
