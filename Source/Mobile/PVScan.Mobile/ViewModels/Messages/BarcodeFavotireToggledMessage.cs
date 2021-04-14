1cc930c95e79c9b46fc4e0d2e3ef703f3fa9162f
Michael <mejevin@gmail.com>
2021-04-14 23:14:10 +0300

    add: barcode favorite toggled message

---
+++ Source/Mobile/PVScan.Mobile/ViewModels/Messages/BarcodeFavotireToggledMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using PVScan.Mobile.Models;
+
+namespace PVScan.Mobile.ViewModels.Messages
+{
+    public class BarcodeFavotireToggledMessage
+    {
+        public Barcode UpdatedBarcode { get; set; }
+    }
+}
