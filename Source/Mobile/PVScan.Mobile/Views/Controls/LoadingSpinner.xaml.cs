225147b8ab23c0d65e26583b9780546e524b56e3
Michael <mejevin@gmail.com>
2021-04-29 14:54:21 +0300

    add: Loading Spinner control

---
+++ Source/Mobile/PVScan.Mobile/Views/Controls/LoadingSpinner.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PVScan.Mobile.Views.Controls
+{
+    public partial class LoadingSpinner : ContentView
+    {
+        public LoadingSpinner()
+        {
+            InitializeComponent();
+
+            StartSpinning();
+        }
+
+        private void StartSpinning()
+        {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    await SpinnerImage.RotateTo(360, 2000, Easing.Linear);
+                    SpinnerImage.Rotation = 0;
+                }
+            });
+        }
+    }
+}
