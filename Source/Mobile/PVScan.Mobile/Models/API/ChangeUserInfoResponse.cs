c1278eae02605db51fcfe35448d7e095a2e4b124
Michael <mejevin@gmail.com>
2021-04-30 15:34:24 +0300

    add: models for API inteaction

---
+++ Source/Mobile/PVScan.Mobile/Models/API/ChangeUserInfoResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PVScan.Mobile.Models.API
+{
+    public class ChangeUserInfoResponse
+    {
+        public int BarcodesScanned { get; set; }
+        public int BarcodeFormatsScanned { get; set; }
+        public double Experience { get; set; }
+        public int Level { get; set; }
+        public string VKLink { get; set; }
+        public string IGLink { get; set; }
+    }
+}
