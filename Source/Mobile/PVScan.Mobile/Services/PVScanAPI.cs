0bb637c4483080556b8f3257f810a8613e82da2b
Michael <mejevin@gmail.com>
2021-04-30 15:35:15 +0300

    add: implementation of basic IPVScanAPI

---
+++ Source/Mobile/PVScan.Mobile/Services/PVScanAPI.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using PVScan.Mobile.Models.API;
+using PVScan.Mobile.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PVScan.Mobile.Services
+{
+    public class PVScanAPI : IPVScanAPI
+    {
+        readonly IIdentityService IdentityService;
+        readonly IHttpClientFactory HttpClientFactory;
+
+        public PVScanAPI(IIdentityService identityService, IHttpClientFactory httpClientFactory)
+        {
+            IdentityService = identityService;
+            HttpClientFactory = httpClientFactory;
+        }
+
+        public async Task<ChangeUserInfoResponse> ChangeUserInfo(ChangeUserInfoRequest req)
+        {
+            var client = HttpClientFactory.ForAPI(IdentityService.AccessToken);
+            var contentToSend = new StringContent(JsonConvert.SerializeObject(req), Encoding.UTF8, "application/json");
+
+            var apiResponse = await client
+                .PostAsync("api/v1/users/change", contentToSend)
+                .WithTimeout(DataAccss.WebRequestTimeout);
+
+            if (!apiResponse.IsSuccessStatusCode)
+            {
+                // Could also throw an exception :)
+                return null;
+            }
+            
+            var responseText = await apiResponse.Content.ReadAsStringAsync();
+            var responseObject = JsonConvert.DeserializeObject<ChangeUserInfoResponse>(responseText);
+
+            return responseObject;
+        }
+
+        public async Task<GetUserInfoResponse> GetUserInfo(GetUserInfoRequest req)
+        {
+            HttpClient httpClient = HttpClientFactory.ForAPI(IdentityService.AccessToken);
+
+            var apiResponse = await httpClient
+                .GetAsync("api/v1/users/current")
+                .WithTimeout(DataAccss.WebRequestTimeout);
+
+            if (!apiResponse.IsSuccessStatusCode)
+            {
+                // Could also throw an exception :)
+                return null;
+            }
+
+            var responseText = await apiResponse.Content.ReadAsStringAsync();
+            var responseObject = JsonConvert.DeserializeObject<GetUserInfoResponse>(responseText);
+
+            return responseObject;
+        }
+    }
+}
