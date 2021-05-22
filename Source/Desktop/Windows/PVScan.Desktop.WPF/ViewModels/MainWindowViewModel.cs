739a0418b1b7a626da553856b940a4fcdceaf347
Michael <mejevin@gmail.com>
2021-05-22 17:19:44 +0300

    fuck window chrome

--- Source/Desktop/Windows/PVScan.Desktop.WPF/ViewModels/MainWindowViewModel.cs
+++ Source/Desktop/Windows/PVScan.Desktop.WPF/ViewModels/MainWindowViewModel.cs
@@ -10,30 +10,8 @@ namespace PVScan.Desktop.WPF.ViewModels
 {
     public class MainWindowViewModel : BaseViewModel
     {
-        IBarcodesRepository BarcodesRepository;
-
-        public MainWindowViewModel(IBarcodesRepository barcodesRepository)
+        public MainWindowViewModel()
         {
-            BarcodesRepository = barcodesRepository;
-
-            BarcodesRepository.Save(new Barcode()
-            {
-                Format = ZXing.BarcodeFormat.QR_CODE,
-                Text = "Niggers!",
-                Favorite = true,
-                ScanTime = new DateTime(2000, 1, 1),
-                ScanLocation = new Coordinate()
-                {
-                    Latitude = 30,
-                    Longitude = 50,
-                },
-            });
         }
-
-        public string Message
-        {
-            get;
-            set;
-        } = "Hello!";
     }
 }
