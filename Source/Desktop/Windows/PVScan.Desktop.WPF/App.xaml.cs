96642ad57f06fd4d7c19be1c63208afdac64ad57
Michael <mejevin@gmail.com>
2021-05-22 14:59:55 +0300

    Wow! First try!!!!!!!!!!!!!!!!!!

--- Source/Desktop/Windows/PVScan.Desktop.WPF/App.xaml.cs
+++ Source/Desktop/Windows/PVScan.Desktop.WPF/App.xaml.cs
@@ -1,9 +1,11 @@
-﻿using PVScan.Desktop.WPF.DI;
+﻿using PVScan.Core;
+using PVScan.Desktop.WPF.DI;
 using PVScan.Desktop.WPF.Views;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,6 +19,11 @@ namespace PVScan.Desktop.WPF
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            DataAccess.Init(
+                Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "PVScan.db3")
+                );
+
             _ = new Bootstrapper();
 
             var mainWindow = Resolver.Resolve<MainWindow>();
--- Source/Desktop/Windows/PVScan.Desktop.WPF/ViewModels/MainWindowViewModel.cs
+++ Source/Desktop/Windows/PVScan.Desktop.WPF/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PVScan.Core.Models;
+using PVScan.Core.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +10,24 @@ namespace PVScan.Desktop.WPF.ViewModels
 {
     public class MainWindowViewModel : BaseViewModel
     {
-        public MainWindowViewModel()
-        {
+        IBarcodesRepository BarcodesRepository;
 
+        public MainWindowViewModel(IBarcodesRepository barcodesRepository)
+        {
+            BarcodesRepository = barcodesRepository;
+
+            BarcodesRepository.Save(new Barcode()
+            {
+                Format = ZXing.BarcodeFormat.QR_CODE,
+                Text = "Niggers!",
+                Favorite = true,
+                ScanTime = new DateTime(2000, 1, 1),
+                ScanLocation = new Coordinate()
+                {
+                    Latitude = 30,
+                    Longitude = 50,
+                },
+            });
         }
 
         public string Message
