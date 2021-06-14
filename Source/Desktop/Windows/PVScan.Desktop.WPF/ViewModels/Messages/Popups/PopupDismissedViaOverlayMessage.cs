2a3502e5cc9bf680ef65b2dca29d87ac1a9f9cc2
Michael <mejevin@gmail.com>
2021-06-15 02:04:20 +0300

    add: PopupDismissedViaOverlayMessage

---
+++ Source/Desktop/Windows/PVScan.Desktop.WPF/ViewModels/Messages/Popups/PopupDismissedViaOverlayMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PVScan.Desktop.WPF.ViewModels.Messages.Popups
+{
+    public class PopupDismissedViaOverlayMessage
+    {
+    }
+}
