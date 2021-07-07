0a1175c53e1b7e5de220e6e8d98ada588c567844
Michael <mejevin@gmail.com>
2021-07-07 19:59:02 +0300

    chore: remove test users from Auth server Config

--- Source/Web/Backend/PVScan.Auth/Config.cs
+++ Source/Web/Backend/PVScan.Auth/Config.cs
@@ -12,42 +12,12 @@ namespace PVScan.Auth
 {
     public static class Config
     {
-        public static List<TestUser> Users =>
-            new List<TestUser>
-            {
-                new TestUser
-                {
-                    SubjectId = "fec0a4d6-5830-4eb8-8024-272bd5d6d2bb",
-                    Username = "Jon",
-                    Password = "jon123",
-                    Claims = new List<Claim>
-                    {
-                        new Claim("given_name", "Jon"),
-                        new Claim("family_name", "Doe"),
-                        //new Claim("role", "Administrator"),
-                    }
-                },
-                new TestUser
-                {
-                    SubjectId = "c3b7f625-c07f-4d7d-9be1-ddff8ff93b4d",
-                    Username = "Steve",
-                    Password = "steve123",
-                    Claims = new List<Claim>
-                    {
-                        new Claim("given_name", "Steve"),
-                        new Claim("family_name", "Smith"),
-                        //new Claim("role", "Tour Manager"),
-                    }
-                }
-            };
-
         public static IEnumerable<IdentityResource> IdentityResources =>
             new List<IdentityResource>
             {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile(),
             };
-
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new List<ApiScope>
