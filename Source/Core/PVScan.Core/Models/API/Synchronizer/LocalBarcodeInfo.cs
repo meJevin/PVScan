8dcf2c547ea864c0ff30ccc6973e0328db9a8148
Michael <mejevin@gmail.com>
2021-07-08 23:36:32 +0300

    add: Synchronize req/res models

---
+++ Source/Core/PVScan.Core/Models/API/Synchronizer/LocalBarcodeInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PVScan.Core.Models.API
+{
+    public class LocalBarcodeInfo
+    {
+        // Id of local device DB
+        public int LocalId { get; set; }
+        public string GUID { get; set; }
+        public string Hash { get; set; }
+        public DateTime LastTimeUpdated { get; set; }
+    }
+
+}
---
+++ Source/Web/Backend/PVScan.API/ViewModels/Synchronizer/SynchronizeRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PVScan.API.ViewModels.Synchronizer
+{
+    public class LocalBarcodeInfo
+    {
+        // Id of local device DB
+        public int LocalId { get; set; }
+        public string GUID { get; set; }
+        public string Hash { get; set; }
+        public DateTime LastTimeUpdated { get; set; }
+    }
+
+    public class SynchronizeRequest
+    {
+        public IEnumerable<LocalBarcodeInfo> LocalBarcodeInfos { get; set; }
+    }
+}
