394c07c68f1dc684a113e35bfc6c85d377b5d16d
Michael <mejevin@gmail.com>
2021-07-08 23:39:07 +0300

    add: IBarcodeSynchronizer in Core project

---
+++ Source/Core/PVScan.Core/Services/Interfaces/IBarcodeSynchronizer.cs
@@ -0,0 +1,14 @@
+﻿using PVScan.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PVScan.Core.Services.Interfaces
+{
+    public interface IBarcodeSynchronizer
+    {
+        event EventHandler SynchorinizedLocally;
+        Task Synchronize(); 
+    }
+}
