74c986b6e9293e67252f6b2bf35b600e3d2ca128
Michael <mejevin@gmail.com>
2021-08-07 15:36:49 +0300

    add: index for GUID and unique

---
+++ Source/Core/PVScan.Core/Migrations/20210807123620_BarcodeIndexAdded_GUID.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PVScan.Core.Migrations
+{
+    public partial class BarcodeIndexAdded_GUID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Barcodes_GUID",
+                table: "Barcodes",
+                column: "GUID",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Barcodes_GUID",
+                table: "Barcodes");
+        }
+    }
+}
