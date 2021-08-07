9f7a1b9795ddebf3f528a06d5c0a58b27153968a
Michael <mejevin@gmail.com>
2021-08-07 15:31:54 +0300

    add: PVScanDbContextFactory

---
+++ Source/Core/PVScan.Core/Services/Interfaces/IPVScanDbContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using PVScan.Core.DAL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PVScan.Core.Services.Interfaces
+{
+    public interface IPVScanDbContextFactory
+    {
+        PVScanDbContext Get();
+    }
+}
