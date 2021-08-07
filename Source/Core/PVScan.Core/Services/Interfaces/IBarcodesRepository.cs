c709630dee754a00d42e87d41e865fdb4bca34fc
Michael <mejevin@gmail.com>
2021-08-07 14:30:53 +0300

    change: IBarcodesRepository now supports CRUD on multiple entities

--- Source/Core/PVScan.Core/Services/Interfaces/IBarcodesRepository.cs
+++ Source/Core/PVScan.Core/Services/Interfaces/IBarcodesRepository.cs
@@ -11,16 +11,21 @@ namespace PVScan.Core.Services.Interfaces
     {
         // Create
         Task<Barcode> Save(Barcode barcode);
+        Task Save(IEnumerable<Barcode> barcodes);
 
         // Read
         Task<IEnumerable<Barcode>> GetAll();
 
         // Delete
         Task Delete(Barcode barcode);
+        Task Delete(IEnumerable<Barcode> barcodes);
+
 
         // Update
         Task Update(Barcode barcode);
+        Task Update(IEnumerable<Barcode> barcodes);
 
         Task<Barcode> FindByGUID(string GUID);
+        Task<IEnumerable<Barcode>> FindByGUID(IEnumerable<string> GUIDs);
     }
 }
